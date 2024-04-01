790c25ae4e0120e0f6acc39eadbff7675e9a1456
LU <>
2024-04-01 23:03:20 +0800

    新增遊戲圖片 新增遊戲開始畫面

--- frontend/src/views/gameView.vue
+++ frontend/src/views/gameView.vue
@@ -1,7 +1,16 @@
 <script setup></script>
 
 <template>
-  <main></main>
+  <!-- 背景 -->
+  <section class="game">
+    
+
+
+    <!-- 開始頁面-->
+    <div class="game_logo" >失落的廚房</div>
+    <button @click="" class="game_start  " >開始遊戲</button>
+
+</section>
 </template>
 
 <style lang="scss" scoped>
