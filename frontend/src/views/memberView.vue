0d049ad437bb2a2855378d8aa607ef57b3a8c475
MMMMMilo <helloapple0321@gmail.com>
2024-04-02 01:03:33 +0800

    新增member頁面

--- frontend/src/views/memberView.vue
+++ frontend/src/views/memberView.vue
@@ -1,9 +1,28 @@
-<script setup></script>
-
 <template>
-  <main></main>
+  <main>
+    <div class="wrap">
+      <memberInfo/>
+    </div>
+  </main>
 </template>
 
+<script setup>
+  import memberInfo from "@/components/memberInfo.vue"  
+
+
+</script>
 <style lang="scss" scoped>
-@import "@/assets/scss/page/member.scss";
+main {
+  width: 100svw;
+  height: 100svh;
+  margin: -8px;
+  background: $primaryColorGreen;
+  background-image: url("https://www.transparenttextures.com/patterns/grid-me.png");
+}
+.wrap {
+  max-width: 360px;
+  //height: calc(100svh - 50px);
+  margin: auto;
+  padding: 40px 20px 0 20px;
+}
 </style>
