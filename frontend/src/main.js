d1808c88d351c82a17a98d7567cd4b0d1c8b7b76
MMMMMilo <helloapple0321@gmail.com>
2024-04-10 20:45:26 +0800

    重新拉

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,10 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 
+<<<<<<< HEAD
+// firebase 初始化
+import "./firebaseConfig";
+=======
 //引入firebase初始化
 import { initializeApp } from "firebase/app";
 //引入firebaseConfig中的SDK代碼
@@ -11,6 +15,7 @@ import moment from "moment";
 
 // 啟用firebase主程式
 initializeApp(firebaseConfig);
+>>>>>>> milo
 
 const app = createApp(App);
 
@@ -18,5 +23,9 @@ app.use(createPinia());
 app.use(router);
 app.config.globalProperties.$moment = moment;
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> milo
 app.mount("#app");
 
