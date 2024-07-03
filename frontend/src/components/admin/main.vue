34d5eae0309fda574f9601e3fc94d3c62b142fd0
LAPTOP-2USOGHOT\baasid <milo_wu@baasid.com.tw>
2024-07-03 09:42:42 +0800

    重新commit2

--- frontend/src/components/admin/main.vue
+++ frontend/src/components/admin/main.vue
@@ -1,6 +1,8 @@
 <script setup>
-import { ref,onMounted,onUnmounted,computed } from "vue";
+import { ref, onMounted, onUnmounted, computed, watch } from "vue";
 import moment from "moment";
+import { useUserStore } from '@/stores/user';
+import { useFirestore } from '../../composable/useFirestore.js'
 
 const currentTime = ref(getTime());
 
@@ -14,50 +16,14 @@ function updateTime() {
 
 const intervalId = setInterval(updateTime, 1000);
 
+const UserStore = useUserStore();
 
-import { db } from "@/firebaseConfig";
-import {collection,onSnapshot,query} from "firebase/firestore";
-
-const documents = ref([]);
-let getData;
-
-onMounted(async () => {
-  //組件掛載完成時
-  const lastestQuery = query(collection(db, "user"));
-  const getData = onSnapshot(lastestQuery, (snapshot) => {
-    //監聽即時資料更新
-    documents.value = snapshot.docs.map((doc) => {
-      return {
-        // uid:doc.uid,
-        creationDate:doc.data().creationDate
-      }
-    });
-    console.log('Processed documents:', documents.value);
-  });
-});
-
-onUnmounted(() => {
-  //組件被銷毀時
-  getData(); // 停止監聽資料
-});
-
-// 抓取目前會員人數
-// const memberNum = ref("0");
-const memberNum = computed(()=>{
-    return documents.value.length;
+onMounted(async()=>{
+    await UserStore.fetchMembers();
 })
-// 轉換日期格式並計數
-const userCounts = documents.value.reduce((acc, user) => {
-    const date = new Date(user.creationDate)
-    // （getMonth()的月份從0開始，0代表一月，因此需要加1）
-    const month = (date.getMonth() + 1).toString().padStart(2, '0');
-    const day = date.getDate().toString().padStart(2, '0');
-    const formattedDate = month + day;
 
-    acc[formattedDate] = (acc[formattedDate] || 0) + 1;
-    return acc;
-}, {});
-const sortedDates = Object.keys(userCounts).sort();
+const memberList = computed(() => UserStore.memberList);
+const memberNum =  computed(() => memberList.value.length)
 
 
 //引入vue-chartjs的設定
@@ -127,63 +93,30 @@ const chartOptions =
             }
         }
     }
-const chartData = {
-    labels:
-    Object.keys(userCounts).sort(),
-    // ['0501','0502','0503','0504','0505','0506','0507',],
-    datasets:[
-        {
-            label:'歷史統計表會員人數',
-            backgroundColor: 'rgba(192,144,121,0.2)',
-            borderColor: 'white',
-            data: 
-            Object.values(userCounts),
-            // [10,20,30,40,35,25,5] 
-        }
-    ]
-}
 
-// 原本的user資料只有使用Authenication，要取出裡面的所有資料要使用付費cloud function功能
-// 所以模擬一個後端資料庫接上firebase-admin SDK把現有的user資料抓到pinia，再全部寫入FireStore
-// 這個只有使用一次，之後的註冊資料會同步寫入firestore
+const chartData = computed(() => {
+    const dateLables = []
+    memberList.value.forEach((item)=>{
+        dateLables.push(moment(item.creationTime).format('MMDD'))  
+    })
 
-// import { useUserStore } from "@/stores/user";
-// const userStore = useUserStore();
-// async function fetchUserList() {
-//     await userStore.fetchUsers();
-// }
-// onMounted(async()=>{
-//     await fetchUserList()
-// })
+    const accDate = dateLables.reduce((acc,date) => {
+        acc[date] = (acc[date] || 0) + 1
+        return acc
+    },{})
 
-// import { db } from "@/firebaseConfig";
-// import {
-//   collection,
-//   addDoc,
-// } from "firebase/firestore";
-
-// const addData = async () => {
-//   if (!userStore.users || userStore.users.length === 0) {
-//     console.log('No data')
-//     // 確認有輸入值，若為空白則不執行
-//     return;
-//   }
-//   userStore.users.forEach(async(user) => {
-//       try {
-//         const docRef = await addDoc(collection(db, "user"), {
-//             uid: user.uid,
-//             email: user.email,
-//             displayName: user.displayName,
-//             disabled: user.disabled,
-//             emailVerified: user.emailVerified,
-//             creationDate: user.creationDate, 
-//         });
-//         console.log("Document written with ID: ", docRef.id); // 送出後會再回傳一個id值
-//       } catch (e) {
-//         console.error("Error adding document: ", e);
-//       } 
-//   })
-// };
+    return {
+        labels:Object.keys(accDate),
+        datasets:[
+            {
+                label:'註冊會員人數',
+                backgroundColor: 'rgba(192,144,121,0.2)',
+                borderColor: 'white',
+                data:Object.values(accDate)
+            }
+        ]
+    }  
+})
 
 </script>
 
@@ -193,13 +126,10 @@ const chartData = {
     <div class="pcSmTitle">
       目前會員人數統計: <span class="primaryColorPink">{{ memberNum }}</span> 人
     </div>
-    <!-- <div id="member_chart"></div> -->
     <div id="member_chart">
       <Bar :options="chartOptions" :data="chartData"></Bar>
     </div>
     <!-- <p class="pcMarkText">歷史會員人數統計表</p> -->
-     <button @click="addData">add user</button>
-      <!-- <div>{{ documents }}</div> -->
   </main>
 </template>
 
--- frontend/src/stores/user.js
+++ frontend/src/stores/user.js
@@ -1,22 +1,41 @@
 import { defineStore } from 'pinia'
-import axios from 'axios';
+import { useFirestore } from '../composable/useFirestore'
+import { watch } from 'vue'
 
-export const useUserStore = defineStore('UserStore',  {
-    state:()=>{
-        return{
-            users:[]
+export const useUserStore = defineStore('UserStore', {
+    state: () => {
+        return {
+            memberList: [],
+            unsubscribe: null,
         }
     },
-    actions:{
-        async fetchUsers() {
+    actions: {
+        //把原本user資料取出來存進firestore(只會用一次)
+        async saveUser() {
             try {
-              const response = await axios.get(`${import.meta.env.VITE_API_URL}/user`);
-              this.users = response.data;
-              console.log(this.users)
+                //有另外建立一個backend資料夾，並使用express模擬後端api(沒有另外推上GitHub)
+                await fetch('http://localhost:3000/saveUsers');
+
             } catch (error) {
-              console.error('Error fetching users:', error);
+                console.error('Error to save user data.', error)
             }
-          }
+        },
+        //從firestore中取出member資料  引入composable中的function
+        fetchMembers() {
+            const { getData, documents } = useFirestore();
+
+            getData("users") 
+
+            //onSnapshot更新的documents是非同步的，要用watch觀測到新的值在給memberList
+            watch(documents,(newVal,oldVal) => {
+                this.memberList = newVal
+            },{ immediate:true })
+        },
+        // cleanUp() {
+        //     if (this.unsubscribe) {
+        //         this.unsubscribe();
+        //     }
+        // }
     }
 
 })
